13e2ae2b01e7c7e544b510d46cae6626c1418105
kevinhinojosa <kevinhinojosa@10.34.146.89>
2017-03-06 08:02:15 -0600

    Progra

---
+++ src/spp/khinojosac/a06/p2/SPPKHinojosaCA06P2.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+//Kevin Gerardo Hinojosa Castellanos    A01411383  IMT
+package spp.khinojosac.a06.p2;
+import java.util.Scanner;
+/**
+ *
+ * @author kevinhinojosa
+ */
+public class SPPKHinojosaCA06P2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+     
+        //variables
+        int num = 0, suma=0;
+
+        while (num >= 0) {
+            num = solicitarDatos();
+            suma = calcularSuma(num, suma);
+        }
+    }
+
+    public static int solicitarDatos() {
+
+        System.out.println(" Ingresa el número:");
+        Scanner sc = new Scanner(System.in);
+        int numero;
+        try{
+        numero = sc.nextInt();
+        }catch(Exception ex){
+            System.out.println(" No es un número entero");
+            return -1;
+        }
+        return numero;
+    }
+
+    public static int calcularSuma(int num, int suma) {
+        suma = suma + num;
+        System.out.println(" Suma:"+ suma);
+        return suma;
+    }
+
+}
